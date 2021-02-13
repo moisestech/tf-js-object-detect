1b3bbc2aef4d49acf708c86f5a6eacf5d166800b
moisestech <m@moises.tech>
2021-02-13 14:26:04 -0500

    reference link for app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import "./styles.css";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
 let project_name = "Tensorflow.js React Object Detect";
 
 console.log(project_name);
+
+// https://www.youtube.com/watch?v=uTdUUpfA83s
