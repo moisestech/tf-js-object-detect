b87842d1f87cfef331ee84f59c36af5e951cc6a2
moisestech <m@moises.tech>
2021-02-14 10:26:18 -0500

    init react react-dom imp App func comp, strict mode

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import App from './App';
 
-import React, { useRef, useState, userEffect } from "react";
-import ReactDOM from "react-dom";
-
-// access to webcam
-import Webcam from "react-webcam";
-
-// for running object detection
-import * as tf from "@tensorflow/tfjs";
-import * as cocossd from "@tensorflow-models/coco-ssd";
-
-// drawing bound-boxes on detected objects
-import { drawRect } from "./utilities"
-
-
-
-let project_name = "Tensorflow.js React Object Detect";
-
-console.log(project_name);
-
-// https://www.youtube.com/watch?v=uTdUUpfA83s
-// https://github.com/nicknochnack/RealTimeObjectDetectionTFJSReact
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
